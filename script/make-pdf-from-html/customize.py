4b48a5af03d15552e5bc091d4d985526c726aba0
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-09-17 18:06:54 +0100

    Support EM and gate da

--- script/make-pdf-from-html/customize.py
+++ script/make-pdf-from-html/customize.py
@@ -16,7 +16,7 @@ def preprocess(i):
     env['CM_ROOT_PATH'] = os.path.join(os.path.expanduser("~"), "go_books")
     env['CM_PDF_PATH'] = os.path.join(env['CM_ROOT_PATH'], "pdfs")
 
-    cmd = f"""wkhtmltopdf --javascript-delay {env['CM_GO_PDF_JS_DELAY']} -T 20mm -B 20mm --header-spacing 6   --title '{env['CM_GO_PDF_TITLE']}' --no-stop-slow-scripts   --load-error-handling ignore  --enable-local-file-access   toc  {os.path.join(env['CM_ROOT_PATH'], env['CM_GO_HTML_FOLDER_NAME'], env['CM_GO_HTML_FILE_NAME'])}  --enable-toc-back-links --zoom 1.0   {os.path.join(env['CM_PDF_PATH'],env['CM_GO_PDF_NAME'])} """
+    cmd = f"""wkhtmltopdf --javascript-delay {env['CM_GO_PDF_JS_DELAY']} -T 20mm -B 20mm --header-spacing 6   --title '{env['CM_GO_PDF_TITLE']}' --no-stop-slow-scripts   --load-error-handling ignore  --enable-local-file-access   toc  {os.path.join(env['CM_ROOT_PATH'], env['CM_GO_HTML_FOLDER_NAME'], env['CM_GO_HTML_FILE_NAME'])}  --enable-toc-back-links --zoom {env['CM_GO_PDF_SCALE']}   {os.path.join(env['CM_PDF_PATH'],env['CM_GO_PDF_NAME'])} """
 
     env['CM_RUN_CMD'] = cmd
 
