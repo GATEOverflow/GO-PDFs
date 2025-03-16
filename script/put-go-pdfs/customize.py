1d477475c46ef454fcec6ea22476461d28029fca
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-03-17 04:27:00 +0530

    Added put-go-pdfs

---
+++ script/put-go-pdfs/customize.py
@@ -0,0 +1,29 @@
+from mlc import utils
+import os
+import subprocess
+
+
+def preprocess(i):
+
+    env = i['env']
+    state = i['state']
+
+    os_info = i['os_info']
+
+    pdf_root = os.path.dirname(env['MLC_PDF_PATH'])
+
+    env['MLC_RUN_CMD'] = f"""rclone sync {pdf_root} go-pdfs-out:"""
+    return {'return': 0}
+
+
+def postprocess(i):
+
+    env = i['env']
+    state = i['state']
+
+    env['MLC_GO_PDFS_OUT'] = os.path.join(os.getcwd(), 'go-pdfs-out')
+    env['MLC_DEPENDENT_CACHED_PATH'] = env['MLC_GO_PDFS_OUT']
+
+    os_info = i['os_info']
+
+    return {'return': 0}
